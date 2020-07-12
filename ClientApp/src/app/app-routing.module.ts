8fe6cb0af02bcd1ef7e11ea96c8ff387bb9471ad
Lennick <lennick.velez@gmail.com>
2020-07-12 11:53:31 -0400

    Fixed routes / added not found

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -5,23 +5,21 @@ import { AuthGuard } from '@app/shared/guards/auth.guard';
 
 import { HomeComponent } from '@app/home/home.component';
 import { LoginComponent } from '@app/login/login.component';
-import { ShellComponent } from '@app/shared/components';
+import { ShellComponent, NotFoundComponent } from '@app/shared/components';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
     component: ShellComponent,
     children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        component: HomeComponent
-      },
       {
         path: 'users',
         canActivate: [AuthGuard],
         loadChildren: () => import('./+users/users.module').then(m => m.UsersModule)
+      },
+      {
+        path: '',
+        component: HomeComponent
       }
     ]
   },
@@ -30,7 +28,7 @@ const routes: Routes = [
     component: LoginComponent
   },
   // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- ClientApp/src/app/shared/components/index.ts
+++ ClientApp/src/app/shared/components/index.ts
@@ -1,3 +1,4 @@
 export * from './shell/shell.component';
 export * from './nav-menu/nav-menu.component';
 export * from './footer/footer.component';
+export * from './not-found/not-found.component';
